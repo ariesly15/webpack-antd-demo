aea2400d043387f5151eb6a9272c01b8bcd8405f
李燚 <aweleey.li@qunar.com>
2018-09-02 23:04:58 +0800

    webpack-merge

---
+++ webpack.common.js
@@ -0,0 +1,63 @@
+/*
+ * @Author: aweleey.li@qunar.com 
+ * @Date: 2018-09-02 20:52:11 
+ * @Last Modified by: aweleey.li@qunar.com
+ * @Last Modified time: 2018-09-02 21:22:24
+ */
+
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, "./dist"),
+        filename: "[name].[chunkhash].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // 正则匹配以 .js 结尾的文件来使用 babel 解析
+                use: ["babel-loader?cacheDirectory=true"], // cacheDirectory是用来缓存编译结果，下次编译加速
+                include: path.join(__dirname, "src") // 需要解析的目录
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            // olimit 8192意思是，小于等于8K的图片会被转成base64编码，直接插入HTML中，减少HTTP请求
+                            limit: 8192
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.join(__dirname, "src/index.html")
+        }),
+        new webpack.HashedModuleIdsPlugin()
+    ],
+    optimization: {
+        runtimeChunk: "single",
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all"
+                }
+            }
+        }
+    }
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,62 +1,22 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/*
+ * @Author: aweleey.li@qunar.com 
+ * @Date: 2018-09-02 20:49:25 
+ * @Last Modified by: aweleey.li@qunar.com
+ * @Last Modified time: 2018-09-02 21:27:59
+ */
 
-module.exports = {
+const path = require("path");
+const WebpackMerge = require("webpack-merge");
+const CommonConfig = require("./webpack.common");
+
+module.exports = WebpackMerge(CommonConfig, {
     devtool: "inline-source-map",
-    output: {
-        path: path.join(__dirname, "./dist"),
-        filename: "[name].[chunkhash].js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // 正则匹配以 .js 结尾的文件来使用 babel 解析
-                use: ["babel-loader?cacheDirectory=true"], // cacheDirectory是用来缓存编译结果，下次编译加速
-                include: path.join(__dirname, "src") // 需要解析的目录
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            // olimit 8192意思是，小于等于8K的图片会被转成base64编码，直接插入HTML中，减少HTTP请求
-                            limit: 8192
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.join(__dirname, "src/index.html")
-        }),
-        new webpack.HashedModuleIdsPlugin()
-    ],
+    mode: "development",
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         historyApiFallback: true,
         //  指定使用一个 host。默认是 localhost。如果你希望服务器外部可访问，写法如下
         host: "0.0.0.0",
         port: 9090
-    },
-    optimization: {
-        runtimeChunk: "single",
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all"
-                }
-            }
-        }
     }
-};
+});
